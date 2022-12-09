eb09ce229558268efd9f136d42f1f54f11174d5a
Mathias Scherer <scherer.mat@gmail.com>
2022-12-09 22:40:30 +0100

    adds fixed path to logfile

--- starter.sh
+++ starter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "postgres" > /tmp/password
 initdb -U postgres -D /tmp/data -A password --pwfile /tmp/password 
-pg_ctl -D /tmp/data -l logfile start
+pg_ctl -D /tmp/data -l /tmp/logfile start
 
 export PGPASSWORD=postgres
 psql -U postgres postgres -c "create database notme;"
